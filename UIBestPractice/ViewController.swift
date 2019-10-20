0f1b80c1e463a54a672298949527e14480d989e9
TomokiIshimine <ishitoki47259@gmail.com>
2019-10-20 21:50:33 +0900

    Initial Commit

---
+++ UIBestPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIBestPractice
+//
+//  Created by 伊志嶺朝輝 on 2019/10/20.
+//  Copyright © 2019 TomRock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
